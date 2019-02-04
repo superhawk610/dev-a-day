026a4f5e8feb6dd969f1816ac27767a181702a42
Aaron Ross <superhawk610@gmail.com>
2019-02-03 21:55:00 -0500

    log human-readable prebuild size

--- articles/index.ts
+++ articles/index.ts
@@ -1,7 +1,15 @@
 import * as path from 'path';
 import * as fs from 'fs';
+import * as bytes from 'bytes';
+
 import { articles } from './articles';
 
 const output = path.join(__dirname, '..', 'src', 'assets', 'articles.json');
 
-fs.writeFileSync(output, JSON.stringify(articles), 'utf-8');
+const json = JSON.stringify(articles);
+const outputSize = bytes(json.length).toLowerCase();
+fs.writeFileSync(output, json, 'utf-8');
+
+console.log();
+console.log(`  -- WROTE ${outputSize} TO articles.json --`);
+console.log();
