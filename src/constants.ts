89de399609c09a47c6b9119423a51ca29fc07dde
Aaron Ross <superhawk610@gmail.com>
2019-02-11 22:30:49 -0500

    move `TAGS` to separate file

--- src/constants.ts
+++ src/constants.ts
@@ -1,45 +1,6 @@
-import { Tag } from './app/models/tag.model';
-
 export const NAVIGATION_ROUTES = [
   { name: 'Articles', path: '/articles' },
   { name: 'Tags', path: '/tags' },
 ];
 
-export const TAGS: { [name: string]: Tag } = {
-  javascript: {
-    id: 1,
-    name: 'javascript',
-  },
-  ocaml: {
-    id: 2,
-    name: 'ocaml',
-  },
-  angular: {
-    id: 3,
-    name: 'angular',
-  },
-  fp: {
-    id: 4,
-    name: 'fp',
-  },
-  go: {
-    id: 5,
-    name: 'go',
-  },
-  rust: {
-    id: 6,
-    name: 'rust',
-  },
-  swift: {
-    id: 7,
-    name: 'swift',
-  },
-  react: {
-    id: 8,
-    name: 'react',
-  },
-  python: {
-    id: 9,
-    name: 'python',
-  },
-};
+export { TAGS } from './tags';
---
+++ src/tags.ts
@@ -0,0 +1,44 @@
+import { Tag } from './app/models/tag.model';
+
+export const TAGS: { [name: string]: Tag } = {
+  javascript: {
+    id: 1,
+    name: 'javascript',
+  },
+  ocaml: {
+    id: 2,
+    name: 'ocaml',
+  },
+  angular: {
+    id: 3,
+    name: 'angular',
+  },
+  fp: {
+    id: 4,
+    name: 'fp',
+  },
+  go: {
+    id: 5,
+    name: 'go',
+  },
+  rust: {
+    id: 6,
+    name: 'rust',
+  },
+  swift: {
+    id: 7,
+    name: 'swift',
+  },
+  react: {
+    id: 8,
+    name: 'react',
+  },
+  python: {
+    id: 9,
+    name: 'python',
+  },
+  devTools: {
+    id: 10,
+    name: 'dev-tools',
+  },
+};
